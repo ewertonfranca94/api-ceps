178bfe15e6779fd22141ce4591fd0f070c272b2d
ewertonfranca94 <ewerton-1994@hotmail.com>
2025-11-06 18:44:44 -0300

    feat(api): implementa GET /api/cep/{cep} com busca no banco e retorno formatado

--- app/Http/Controllers/CepController.php
+++ app/Http/Controllers/CepController.php
@@ -1,20 +1,41 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use App\Http\Requests\CepLookupRequest;
-use Illuminate\Http\Request;
+use App\Services\AddressService;
+use Illuminate\Http\JsonResponse;
 
 
 class CepController extends Controller
 {
-    public function show(CepLookupRequest $request)
+    protected AddressService $addressService;
+
+    public function __construct(AddressService $addressService)
     {
-        // CEP Validado
-        $cep = $request->validated()['cep'];
+        $this->addressService = $addressService;
+    }
+
+    public function show(CepLookupRequest $request): JsonResponse
+    {
+    // CEP já validado e normalizado pelo FormRequest
+    $cep = $request->validated()['cep'];
+
+    $address = $this->addressService->findByCep($cep);
+
+    if (!$address){
+        return response()->json([
+            'message' => 'CEP não encontrado no banco de dados'
+        ], 404);
+    }
+    // Formata o CEP com hífen antes de retornar
+        $cepFormatado = substr($address->cep, 0, 5) . '-' . substr($address->cep, 5);
 
         return response()->json([
-            'message' => 'CEP validado com sucesso',
-            'cep' => $cep,
+            'logradouro'     => $address->logradouro,
+            'bairro'         => $address->bairro,
+            'localidade_uf'  => "{$address->localidade}/{$address->uf}",
+            'cep'            => $cepFormatado,
         ]);
     }
 }
